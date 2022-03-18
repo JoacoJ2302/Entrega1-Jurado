a0dead4352157ce3703a7f84e508c0da144a659f
Joaquin Jurado <joaquinjurado99@gmail.com>
2022-03-18 15:10:46 -0300

    Arreglos en la estetica del codigo

--- social_django/social/views.py
+++ social_django/social/views.py
@@ -8,18 +8,20 @@ from django.contrib.auth.decorators import login_required
 
 def feed(request):
     posts = Post.objects.all()
-    context = {
-        'posts' : posts,
-    }
+    context = {'posts' : posts,}
+    
     return render (request, 'social/feed.html', context)
 
 def register(request):
+    
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+        
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
             messages.success(request, f'Usuario {username} ha sido creado')
+            
             return redirect('feed')
     else:
         form = UserRegisterForm()
@@ -31,6 +33,7 @@ def register(request):
 
 def profile(request, username=None):
     current_user = request.user
+    
     if username and username != current_user.username:
         user = User.objects.get(username=username)
         posts = user.posts.all()
@@ -41,12 +44,14 @@ def profile(request, username=None):
     return render (request, 'social/profile.html', {'user':user, 'posts':posts})
 
 def about(request):
+    
     return render (request, 'social/about.html')
 
 
 @login_required
 def post(request):
     current_user = get_object_or_404(User, pk=request.user.pk)
+    
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
@@ -57,6 +62,7 @@ def post(request):
             return redirect('feed')
     else:
         form = PostForm()
+    
     return render (request, 'social/post.html', {'form' : form,})
 
 def follow(request, username):
@@ -66,6 +72,7 @@ def follow(request, username):
     rel = Relationship(from_user = current_user, to_user=to_user_id)
     rel.save()
     messages.success(request, f'Sigues a {username}')
+    
     return redirect('feed')
 
 def unfollow(request, username):
@@ -75,6 +82,7 @@ def unfollow(request, username):
     rel = Relationship.objects.filter(from_user=current_user.id, to_user=to_user_id).get()
     rel.delete()
     messages.success(request, f'Ya no sigues a {username}')
+    
     return redirect('feed')
 
 def search(request):
@@ -86,4 +94,5 @@ def search(request):
         users = User.objects.all()
 
     form = SearchProfile()
+    
     return render (request, 'social/search.html', {'form':form, 'users':users})
